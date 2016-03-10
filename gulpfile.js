8afe8f9a66f98d3b16fb989f98e7df4554b8d9b4
Daniel Hoffmann <daniel.hoffmann@globalmouth.com>
2016-03-10 09:17:59 -0300

    Fixed reverted bugfix for static files

--- gulpfile.js
+++ gulpfile.js
@@ -199,7 +199,8 @@
             '!./src/i18n/**',
             '!./src/i18n/'
          ])
-         .pipe(gulp.dest('./' + compiledTemp));
+         .pipe(gulp.dest('./' + compiledTemp))
+         .pipe(gulp.dest('./dist/'));
    });
 
    /**
