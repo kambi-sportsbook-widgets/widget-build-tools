9a2241de0c75355377b58bce65e9e866e8755174
Daniel Hoffmann <daniel.hoffmann@globalmouth.com>
2016-10-03 12:24:44 +0200

    formatting and replaced const statements with var

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,8 +1,9 @@
-const path = require('path');
-const validate = require('webpack-validator');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const fs = require('fs');
+var path = require('path');
+var validate = require('webpack-validator');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var fs = require('fs');
 
 module.exports = validate({
    entry: {
@@ -13,28 +14,60 @@ module.exports = validate({
          { test: /src\/.*.js$/, exclude: /node_modules/, loader: `eslint-loader?{configFile:"${path.join(__dirname, '.eslintrc')}"}` }
       ],
       loaders: [
-         { test: /\.svg/, loader: 'svg-url-loader' },
-         { test: /(\.ttf|\.woff2?|\.eot)/, loader: 'url-loader' },
-         { test: /.js$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: ['es2015'] } },
-         { test: /\.scss$/, loaders: ['style-loader', 'css-loader?sourceMap', 'sass-loader?sourceMap'] },
-         { test: /\.html/, loader: 'html-loader' },
-         { test: /\.json$/, loader: 'json-loader' }]
+         {
+            test: /\.svg/,
+            loader: 'svg-url-loader' },
+         {
+            test: /(\.ttf|\.woff2?|\.eot)/,
+            loader: 'url-loader' },
+         {
+            test: /.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+               presets: [require.resolve('babel-preset-es2015')]
+            }
+         },
+         {
+            test: /\.scss$/,
+            loaders: [
+               'style-loader',
+               'css-loader?sourceMap', 'sass-loader?sourceMap'
+            ]
+         },
+         {
+            test: /\.html/,
+            loader: 'html-loader'
+         },
+         {
+            test: /\.json$/,
+            loader: 'json-loader'
+         }]
    },
-   resolveLoader: { root: fs.existsSync(path.join(__dirname, "node_modules")) ? path.join(__dirname, "node_modules") : path.join(process.cwd(), 'node_modules') },
+   resolveLoader: {
+      root: fs.existsSync(
+         path.join(__dirname, 'node_modules')) ?
+            path.join(__dirname, 'node_modules') :
+            path.join(process.cwd(), 'node_modules'
+         )
+   },
    devtool: 'source-map',
    output: {
       path: path.resolve(process.cwd(), 'dist'),
       filename: 'js/[name].js'
    },
-   devServer: {
-      contentBase: './src',
-      https: true
-   },
    resolve: {
       extensions: ['', '.js', '.json', '.scss', '.html']
    },
    plugins: [
-      new HtmlWebpackPlugin({template: 'src/index.html'}),
+      new webpack.DefinePlugin({
+         'process.env': {
+            NODE_ENV: process.env.NODE_ENV
+         }
+      }),
+      new HtmlWebpackPlugin({
+         template: 'src/index.html'
+      }),
       new CopyWebpackPlugin([
          { from: './src/i18n', to: 'i18n' },
          { from: path.join(__dirname, 'widget_config', 'mockSetupData.json'), to: '.' }
