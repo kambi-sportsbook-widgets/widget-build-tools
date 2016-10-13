d9ded469d408cc4caf53ab0c9e7811d1593fb205
Daniel Hoffmann <daniel.hoffmann@globalmouth.com>
2016-10-13 14:03:39 +0200

    Added a slightly more complex example to the template

--- template/index.js
+++ template/index.js
@@ -1,10 +1,39 @@
-import { coreLibrary, widgetModule } from 'widget-core-library';
+import {
+   coreLibrary,
+   widgetModule,
+   offeringModule
+} from 'widget-core-library';
 import './index.html';
 import './app.scss';
 
 coreLibrary.init({
    // default arguments
-}).then(() => {
+   title: '${projectName}'
+}).then(function () {
    // retrieved arguments
-   const args = coreLibrary.args;
+   var args = coreLibrary.args;
+
+   // receiving the title through this widget arguments
+   document.getElementById('title').innerText = args.title;
+
+   offeringModule.getLiveEvents()
+      .then(function (data) {
+         // showing basic information of the first live event we get
+         var ev = data.events[0].event;
+         var liveData = data.events[0].liveData;
+         var homeHtml = document.getElementById('home-info');
+         var awayHtml = document.getElementById('away-info');
+         var matchInfo = document.getElementById('match-info');
+         matchInfo.innerText = ev.name;
+         homeHtml.innerText = ev.homeName + ' - ' + liveData.score.home;
+         awayHtml.innerText = liveData.score.away + ' - ' + ev.awayName;
+         // make the widget the same height as the body
+         widgetModule.adaptWidgetHeight();
+      }).catch(function(err) {
+         // could not fetch the data
+         // or an error happened while parsing it
+         console.error(err);
+         // making widget remove itself from the sportsbook
+         widgetModule.removeWidget();
+      });
 });
