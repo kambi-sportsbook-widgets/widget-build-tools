dfb8f8d22a11bb760b14c2206ae5d5f2f9e306f2
Daniel Hoffmann <daniel.hoffmann@globalmouth.com>
2017-02-23 13:38:42 +0100

    Test script now runs node. Added jest watch and debug features

--- bin/test.js
+++ bin/test.js
@@ -9,44 +9,65 @@ const chalk = require('chalk'),
 const test = ({ options }) => {
    const buildToolsPath = path.resolve(__dirname, '../');
    const subjectPath = process.cwd();
-   const cliPath = 'npm';
-
    const config = {
+      /*
+      this object is serialized into a CLI command
+      that is why we have to use so many backslashes in the regular expressions
+      */
       moduleDirectories: [
          'node_modules',
          path.resolve(buildToolsPath, 'node_modules')
       ],
       moduleNameMapper: {
-         [ '\\\\.(s?css|less)$' ]: "identity-obj-proxy"
+         [ '\\.(s?css|less)$' ]: "identity-obj-proxy"
       },
       rootDir: subjectPath,
-      testRegex: '(/tests/.*|(\\\\.|/)(test|spec))\\\\.jsx?$',
+      testRegex: '(./tests/.*|(\\.|/)(test|spec))\\.jsx?$',
+
       transform: {
-         ['\\\\.jsx?']: path.resolve(buildToolsPath, './jest/transform.js')
+         ['\\.jsx?']: 'babel-jest'
       },
       transformIgnorePatterns: [
-         '/node_modules/(?!kambi-widget-core-library)(?!kambi-widget-components)'
+         '/node_modules/(?!kambi-widget-core-library)(?!kambi-widget-components)' // transforms core-library and widget-components as well but ignores the rest of /node_modules/
       ]
    };
 
-   const params = [ 'run', 'jest', '--', '--colors' ];
+   const jestParams = ['--colors'];
+   const nodeParams = [];
 
    if (options.updateSnapshot) {
-      params.push('-u');
+      jestParams.push('-u');
    }
 
    if (options.verbose) {
-      params.push('--verbose');
+      jestParams.push('--verbose');
    }
 
    if (options.coverage) {
-      params.push('--coverage');
+      jestParams.push('--coverage');
       config.coverageDirectory = path.resolve(subjectPath, 'coverage');
    }
 
-   params.push(`--config=${JSON.stringify(config)}`);
+   if (options.watch) {
+      jestParams.push('--watch');
+   }
 
-   return exec(cliPath, params, { cwd: buildToolsPath });
+   if (options.debug) {
+      // Breakpoints are currently not working (tested on nodejs 7.6)
+      // see https://github.com/facebook/jest/issues/1652
+      jestParams.push('--runInBand'); // makes the tests run serially instead of in parallel
+      nodeParams.push('--inspect');
+      nodeParams.push('--debug-brk');
+   }
+
+   jestParams.push(`--config=${JSON.stringify(config)}`);
+
+   return exec('node',
+      nodeParams
+         .concat(['./node_modules/.bin/jest'])
+         .concat(jestParams),
+      { cwd: subjectPath }
+   );
 };
 
 test.config = {
@@ -55,7 +76,9 @@ test.config = {
    options: [
       ['', 'coverage', 'Indicates that test coverage information should be collected and reported in the output.'],
       ['u', 'updateSnapshot', 'Use this flag to re-record snapshots.'],
-      ['', 'verbose', 'Display individual test results with the test suite hierarchy']
+      ['', 'verbose', 'Display individual test results with the test suite hierarchy'],
+      ['w', 'watch', 'Watchs for changes to files and re-run the tests'],
+      ['d', 'debug', 'Runs the tests in debug mode. Requires Nodejs version 6.3 or higher. It is better to run this in conjuction with the watch mode. As of Nodejs version 7.6 breakpoints and debugger statements are ignored. This is a bug in Nodejs']
    ]
 };
 
--- jest/transform.js
+++
@@ -1,8 +0,0 @@
-const babelJest = require('babel-jest');
-
-module.exports = babelJest.createTransformer({
-   presets: [
-      'babel-preset-es2015',
-      'babel-preset-react'
-   ]
-});
