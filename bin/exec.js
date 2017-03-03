3256132fe4c75c763f93be8a9457a29dd15a9b66
Daniel Hoffmann <daniel.hoffmann@globalmouth.com>
2017-03-03 11:05:55 +0100

    stdin is now redirected to child process as well

--- bin/exec.js
+++ bin/exec.js
@@ -11,11 +11,8 @@ const exec = (cmd, params, options) => {
    return new Promise((resolve, reject) => {
       console.log(`> ${cmd} ${params.join(' ')}`);
 
+      options.stdio = 'inherit';
       const child = childProcess.spawn(cmd, params, options);
-
-      child.stdout.on('data', (data) => process.stdout.write(data));
-
-      child.stderr.on('data', (data) => process.stderr.write(data));
 
       child.on('close', (code) => {
          if (code == 0) {
