315f1927f567b0fa505ba1ddd3af0135bb3dea25
Karol Maciaszek <karol@globalmouth.com>
2017-06-06 16:24:39 +0200

    Fixed NODE_ENV detection

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const merge = require('webpack-merge');
 
 
-if (['development', 'production'].indexOf(process.env.NODE_ENV)) {
+if (['development', 'production'].indexOf(process.env.NODE_ENV) === -1) {
    throw new Error('Environment variable NODE_ENV not set, please set it to either "production" or "development"');
 }
 
